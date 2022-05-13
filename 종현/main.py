727d8460acbaa7a94dedd9ffbf001cc75e82052b
한종현 <93965016+santaj91@users.noreply.github.com>
2022-05-13 17:46:31 +0900

    확인

--- 종현/main.py
+++ 종현/main.py
@@ -5,30 +5,10 @@ import glob
 import dlib
 from keras.models import load_model
 
-
-def load_imgs(img_path,size):
-    result=[]
-    imgs_path_list= [x for x in glob.glob(img_path + '/*.jpg')]
-
-    for img_path in imgs_path_list:
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        img = cv2.resize(img,size)
-        result.append(img)
-    return result # 리스트 형태로 여러장 이미지 반환
-
-def crop(image,input_size):# 이미지 하나, 모델에 들어갈 사이즈
-    imgs=[]
-    detector = dlib.get_frontal_face_detector()
-    faces = detector(image)
-    for face in faces:
-        face_img=image[face.top():face.bottom(), face.left():face.right()]
-        face_img=cv2.resize(face_img,dsize=input_size)
-        face_input=face_img.copy().reshape((input_size[1], input_size[0], 3)).astype(np.float32)/255.
-        imgs.append(face_input)
-    return imgs# 얼굴 여러개
+from function import crop , load_imgs
 
 
-# load the model
+##----------------------------------------------- load the model-----------------------------------------------------------------##
 model = load_model('./model/facenet_keras.h5')
 # summarize input and output shape
 print(model.inputs)
@@ -36,6 +16,41 @@ print(model.outputs)
 model.load_weights('./model/facenet_keras_weights.h5')
 
 
+size = (448,448) # 나중에 수정  
+input_size= (160,160)
+
+##---------------------------------------------Load images ( n <=5 )--------------------------------------------------------------##
 
 
+
+
+
+
+
+##--------------------------------------------------------------------------------------------------------------------------------##
+
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+while cap.isOpened(): # 캡처 객체 초기화 확인 cap 객체가 지정한 파일이 정상적으로 초기화 -> True
+    ret, img_ori = cap.read() # 다음 프레임 읽기 / 프레임 잘 읽었으면 ret = True , img_ori는 프레임 이미지
+
+    if not ret:
+        break
+
+    img = img_ori.copy
+    img = cv2.resize(img,size)
+    video_crops = crop.crop(img,input_size)# 리스트 형태의 nomalizing 된 cropped face image
+    
+    for video_face in video_crops: # 여러개일 가능성도 있기 떄문에 
+        prediction_video=model.predict(video_face) # video에 나온 얼굴
+
+
+
+
+
+
+
+    if cv2.waitKey(1) == ord('q'): # 1ms 의 지연을 주면서 화면에 표시, q 누르면 break
+        break
 
